f6c983329a6b266642df8a666fe032781793ac39
juliamsmith <jmsmith20@uchicago.edu>
2018-08-21 07:29:59 -0500

    small changes

--- Meta/Julias_ideas/Meta_Driver.py
+++ Meta/Julias_ideas/Meta_Driver.py
@@ -12,13 +12,15 @@ def vary_params(dim_vec, m_prop_vec, RB_time_vec, num_sims):
                 RB_time_val = RB_time_vec[k] 
                 #in_write writes input files and creates a folder called conditions_name with param and res folders inside
                 [in_titles, out_titles, conditions_name] = in_write(dim_val, m_prop_val, RB_time_val, num_sims)
+                
+                
+                #writer puts them in a input folder.. try to run one input file tomorrow
                 for l in range(len(in_titles)):
                     in_title = in_titles[l]
                     out_title = out_titles[l]
-                    os.system("python3 bowerbird_prog.py {}".format(in_title))
+                    #os.system("python3 bowerbird_prog.py {}".format(in_title))
                     # incorporate batch stuff properly -- BnS(script, JobName, queue=Q)
                     # I believe 1000 sims (or one set of conditions) is a batch
-                    shutil.move(in_title, "{}/parameters".format(conditions_name))
-                    shutil.move(out_title, "{}/results".format(conditions_name))
-                
-                
+                    #perhaps the ideal script is the 1000sims (so just outside of this for loop)
+                    #shutil.move(in_title, "{}/parameters".format(conditions_name))
+                    #shutil.move(out_title, "{}/results".format(conditions_name))
